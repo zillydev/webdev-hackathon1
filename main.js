16d21333d01f9a4d902eb9ed1117b0b95d45c951
Zaid Sharieff <zaidsharieff09@gmail.com>
2023-12-03 21:38:30 +0530

    Refactoring

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var aboutUsSection = document.getElementById('section2');
-    var scrollButton = document.getElementById('scroll-button');
-    scrollButton.addEventListener('click', function () {
-        aboutUsSection.scrollIntoView({behavior: "smooth"});
-    });
-
-    var homeSection = document.getElementById('dashboard');
+    var homeSection = document.getElementById('home');
     var homeButton = document.getElementById('nav-btn-home');
     homeButton.addEventListener('click', function () {
         homeSection.scrollIntoView({behavior: "smooth"});
     });
 
-    var aboutUsSection = document.getElementById('section2');
+    var aboutUsSection = document.getElementById('about-us');
     var aboutUsButton = document.getElementById('nav-btn-aboutus');
     aboutUsButton.addEventListener('click', function () {
         aboutUsSection.scrollIntoView({behavior: "smooth"});
+    });
+
+    var servicesSection = document.getElementById('services');
+    var servicesButton = document.getElementById('nav-btn-services');
+    servicesButton.addEventListener('click', function () {
+        servicesSection.scrollIntoView({behavior: "smooth"});
+    });
+
+    var portfolioSection = document.getElementById('portfolio');
+    var portfolioButton = document.getElementById('nav-btn-portfolio');
+    portfolioButton.addEventListener('click', function () {
+        portfolioSection.scrollIntoView({behavior: "smooth"});
+    });
+
+    var teamSection = document.getElementById('team');
+    var teamButton = document.getElementById('nav-btn-team');
+    teamButton.addEventListener('click', function () {
+        teamSection.scrollIntoView({behavior: "smooth"});
     });
   });
